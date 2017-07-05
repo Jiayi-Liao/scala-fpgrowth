aaad23900080b090b2a4cc69914b8863bf86d40a
Liao Jiayi <liaojiayi@ip-192-168-37-13.cn-north-1.compute.internal>
2017-07-05 21:45:40 +0800

    ss

--- src/main/scala/com/wind/FPGrowth.scala
+++ src/main/scala/com/wind/FPGrowth.scala
@@ -9,7 +9,7 @@ import scala.io.Source
 object FPGrowth extends App{
 
   val is = ClassLoader.getSystemResourceAsStream("data1.csv")
-  val lines = Source.fromInputStream(is).getLines
+  val lines = Source.fromInputStream(is).getLines.toArray
   for (line <- lines) println(line)
 
   // support
@@ -22,29 +22,50 @@ object FPGrowth extends App{
     })
   })
 
-  // remove item whose count < 2
-  val filted_itemCount = itemCount.filter(v => v._2 > 2)
+  // remove item whose count < support
+  val filted_itemCount = itemCount.filter(v => v._2 > support)
 
   // FPgrowth
-
+  val node = buildTree(lines.toArray)
+  println("Finish!")
 
 
   def buildTree(lines: Array[String]): FPNode = {
-    val root = FPNode("root", 0, Array())
-    lines.foreach( line => {
-      var tmpNode = root
-      var items = line.split(",")
-      while (items.length > 0){
-        val repeat = tmpNode.child.map(_.name).intersect(items)
-        items = if (repeat.length > 0) {
-          tmpNode.child.find(_.name.equals(repeat.head))
-          items.filter(_.equals(repeat.head))
-        } else {
-          items.tail
-        }
-      }
+    val root: FPNode = lines.foldLeft[FPNode](FPNode("root", 0, Array()))((node, line)=>{
+      val items = line.split(",")
+      recursiveInsert(node, items)
     })
+    root
+  }
+
+  def recursiveInsert(node: FPNode, items: Array[String]): FPNode = {
+    if(items.length > 0){
+      val repeat = node.child.map(_.name).intersect(items)
+      val (rChildNode,rItems) = if (repeat.length > 0) {
+        (node.child.find(_.name.equals(repeat.head)).get.addCount(), items.filter(!_.equals(repeat.head)))
+      } else {
+        (FPNode(items.head, 1, Array()), items.tail)
+      }
+      val childNode = recursiveInsert(rChildNode, rItems)
+      if (node.child.exists(_.name.equals(childNode.name))){
+        val index = node.child.indexOf(node.child.find(_.name.equals(childNode.name)).get)
+        node.child.update(index, childNode)
+        node
+      } else {
+        node.addChild(childNode)
+      }
+    } else {
+      node
+    }
   }
 }
 
-case class FPNode(name: String, cnt: Int, child: Array[FPNode])
+case class FPNode(name: String, cnt: Int, child: Array[FPNode]) {
+
+  def addCount() : FPNode = {
+    FPNode(name, cnt+1, child)
+  }
+  def addChild(node: FPNode) : FPNode = {
+    FPNode(name, cnt, child :+ node)
+  }
+}
